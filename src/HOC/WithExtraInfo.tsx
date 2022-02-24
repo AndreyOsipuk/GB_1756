b3a31922eada8bd8c406f6fb06eb0d40dcb9ca3e
Andrey Osipuk <a.osipuk@okko.tv>
2022-02-25 02:12:47 +0400

    fix warnings

--- src/HOC/WithExtraInfo.tsx
+++ src/HOC/WithExtraInfo.tsx
@@ -8,7 +8,8 @@ export function WithExtraInfo<P>(
   WrappedComponent: React.ComponentType<P & ExtraInfoType>
 ) {
   const ComponentWithExtraInfo = (props: P) => {
-    const [extraInfo, setExtraInfo] = useState('important data');
+    const [extraInfo, setExtraInfo] = useState('');
+    setExtraInfo('important data');
     return <WrappedComponent {...props} extraInfo={extraInfo} />;
   };
   return ComponentWithExtraInfo;
